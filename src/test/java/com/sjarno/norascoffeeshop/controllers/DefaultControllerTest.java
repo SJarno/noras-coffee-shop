916e85b619bda05803ebf2bacf5e6cd0f27fdbbb
SJarno <jarno.saastamoinen@hotmail.com>
2022-02-14 17:10:06 +0200

    jacoco plugin and tests

---
+++ src/test/java/com/sjarno/norascoffeeshop/controllers/DefaultControllerTest.java
@@ -0,0 +1,15 @@
+package com.sjarno.norascoffeeshop.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class DefaultControllerTest {
+    
+    @Test
+    void testServerGreeting() {
+
+    }
+}
