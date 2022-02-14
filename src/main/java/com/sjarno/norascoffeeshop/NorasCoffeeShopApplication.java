83e2955055289d5d18ccfd0d8eb6a14e0a8bbcc1
SJarno <jarno.saastamoinen@hotmail.com>
2022-02-14 11:39:38 +0200

    inital

---
+++ src/main/java/com/sjarno/norascoffeeshop/NorasCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.sjarno.norascoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorasCoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorasCoffeeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjarno/norascoffeeshop/NorasCoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjarno.norascoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorasCoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
