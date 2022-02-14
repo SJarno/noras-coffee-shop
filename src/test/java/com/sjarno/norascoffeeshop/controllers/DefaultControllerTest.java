5cf20c17cb86f33bd251986d8445aa7d4513b07b
SJarno <jarno.saastamoinen@hotmail.com>
2022-02-14 17:28:44 +0200

    test case for default controller

--- src/test/java/com/sjarno/norascoffeeshop/controllers/DefaultControllerTest.java
+++ src/test/java/com/sjarno/norascoffeeshop/controllers/DefaultControllerTest.java
@@ -1,15 +1,34 @@
 package com.sjarno.norascoffeeshop.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DefaultControllerTest {
-    
-    @Test
-    void testServerGreeting() {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void testServerGreeting() throws Exception {
+        this.mockMvc.perform(get("/greet")).andExpect(status().isOk());
+
+        MvcResult result = this.mockMvc.perform(get("/greet"))
+            .andExpect(status().isOk()).andReturn();
+        String jsonContent = "{\"greet\":\"Hi, and welcome! The site is currently under construction, but stay tuned for upcoming site.\"}";
+        assertEquals(jsonContent, result.getResponse().getContentAsString());
     }
 }
