c69c0da863b96564ed767b2c7e0249d0062f2a85
SJarno <jarno.saastamoinen@hotmail.com>
2022-03-03 18:26:58 +0200

    to production

--- src/test/java/com/sjarno/norascoffeeshop/services/UserAccountServiceTest.java
+++ src/test/java/com/sjarno/norascoffeeshop/services/UserAccountServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
